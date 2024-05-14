801e2be03e60bb672e637aad8e3dd855a68240b6
Alexandr Busuek <101875725+busuek@users.noreply.github.com>
2024-05-14 17:59:28 +0300

    Add files via upload

---
+++ network/terraform/variables.tf
@@ -0,0 +1,41 @@
+###cloud vars
+variable "token" {
+  type        = string
+  description = "OAuth-token; https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token"
+}
+
+variable "cloud_id" {
+  type        = string
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
+}
+
+variable "folder_id" {
+  type        = string
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
+}
+
+variable "default_zone" {
+  type        = string
+  default     = "ru-central1-a"
+  description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
+}
+
+variable "vpc_name" {
+  type        = string
+  default     = "network1"
+  description = "VPC network&subnet name"
+}
+
+variable "cidr_subnet_public" {
+  type        = list(string)
+  default     = ["192.168.10.0/24"]
+  description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
+}
+
+variable "lamp-instance-image-id" {
+  default = "fd827b91d99psvq5fjit"
+}
+
+variable "public_key" {
+  type    = string
+  default = ""
